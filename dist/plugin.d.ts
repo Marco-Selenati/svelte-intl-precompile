3bb3fe59ba280d748f723310e897835db933fc5e
Nicolas Seiler <nicolas8991@gmail.com>
2022-10-29 21:55:33 +0700

    Add dist folder

---
+++ dist/plugin.d.ts
@@ -0,0 +1,4 @@
+import type { Plugin } from "vite";
+export declare function transformCode(code: string, options: Record<string, any>): string | null | undefined;
+declare const _default: (localesRoot?: string) => Plugin;
+export default _default;
