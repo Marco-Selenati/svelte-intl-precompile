18d7490b5e4cdff2da45a62ff0fa1c20ab27f0d1
Marco Selenati <marcoselenati@gmail.com>
2022-12-20 00:30:25 +0700

    Use vite for build

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      formats: ["es"],
+      entry: {
+        index: resolve(__dirname, "src/index.ts"),
+        plugin: resolve(__dirname, "src/plugin.ts"),
+      },
+    },
+  },
+});
